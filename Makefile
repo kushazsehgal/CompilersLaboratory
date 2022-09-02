eeec4ccc4fab61b8035eaa30f4894d447ac1ab7e
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2022-09-02 10:33:28 +0530

    Added parser and makefile

---
+++ Makefile
@@ -0,0 +1,11 @@
+output.txt: parser trial_test.c 
+	./parser < trial_test > output.txt
+
+parser: lex.yy.c
+	gcc lex.yy.c -lfl -o parser
+
+lex.yy.c: flex_tool.l parser.c
+	flex flex_tool.l parser.c
+
+clean: 
+	rm parser lex.yy.c output.txt
---
+++ parser.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include "definitions.h"
+extern char* yytext;
+extern int yylex();
+
+int main(){
+    int token;
+    while(token == yylex()){
+        switch(token){
+            case KEYWORD:
+                printf("<KEYWORD, %d, %s>\n", token, yytext); 
+            break;
+            case IDENTIFIER: 
+                printf("<IDENTIFIER, %d, %s>\n", token, yytext); 
+                break;
+            case INTEGER_CONST: 
+                printf("<INTEGER_CONSTANT, %d, %s>\n", token, yytext); 
+                break;
+            case FLOAT_CONST: 
+                printf("<FLOAT_CONSTANT, %d, %s>\n", token, yytext); 
+                break;
+            case CHAR_CONST: 
+                printf("<CHARACTER_CONSTANT, %d, %s>\n", token, yytext); 
+                break;
+            case STRING_LITERAL: 
+                printf("<STRING_LITERAL, %d, %s>\n", token, yytext); 
+                break;
+            case PUNCTUATOR: 
+                printf("<PUNCTUATOR, %d, %s>\n", token, yytext); 
+                break;
+            case MULTI_LINE_COMMENT: 
+                printf("<MULTI_LINE_COMMENT, %d>\n", token);  
+                break;
+            case SINGLE_LINE_COMMENT: 
+                printf("<SINGLE_LINE_COMMENT, %d>\n", token); 
+                break;
+            default:
+                printf("<INVALID_TOKEN, %d, %s>\n", token, yytext);
+                break;
+        }
+    }
+}
